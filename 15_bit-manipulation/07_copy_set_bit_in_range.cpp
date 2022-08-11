7e2fb119da6733adff0c08530b4ff02759dda2a5
Sri Ritwik <56584349+sriritwik3@users.noreply.github.com>
2022-08-11 09:23:24 +0530

    Update 07_copy_set_bit_in_range.cpp

--- 15_bit-manipulation/07_copy_set_bit_in_range.cpp
+++ 15_bit-manipulation/07_copy_set_bit_in_range.cpp
@@ -17,4 +17,22 @@ int newNum(int x, int y, int l, int r) {
     return x;
 }
 
+void copySetBits(unsigned &x, unsigned y,
+                 unsigned l, unsigned r)
+{
+    // l and r must be between 1 to 32
+    if (l < 1 || r > 32)
+        return ;
+ 
+    // get the length of the mask
+    int maskLength = (1ll<<(r-l+1)) - 1;
+ 
+    // Shift the mask to the required position
+    // "&" with y to get the set bits at between
+    // l ad r in y
+    int mask = ((maskLength)<<(l-1)) & y ;
+    x = x | mask;
+}
+ 
+
 // ----------------------------------------------------------------------------------------------------------------------- //
